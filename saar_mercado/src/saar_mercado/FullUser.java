d8f73d2a4cbecbd61484f4092735df25257a062f
santisan03 <santi.usf@hotmail.com>
2017-01-09 19:10:50 -0300

    Finalize basic User

--- saar_mercado/src/saar_mercado/FullUser.java
+++ saar_mercado/src/saar_mercado/FullUser.java
@@ -5,4 +5,6 @@ public class FullUser extends User{
 	public FullUser(String usuario, String contraseña, String nombre, String apellido) {
 		super(usuario, contraseña, nombre, apellido);
 	}
+	
+	
 }
---
+++ saar_mercado/src/saar_mercado/SimpleUser.java
@@ -0,0 +1,8 @@
+package saar_mercado;
+
+public class SimpleUser extends User {
+	
+	public SimpleUser (String usuario, String contraseña, String nombre, String apellido) {
+		super(usuario, contraseña, nombre, apellido);
+	}
+}
